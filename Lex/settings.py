0d188caff025ae1f21776573d379550b61947d67
Franco Sandoval <francosandoval003@gmail.com>
2021-06-07 16:39:45 -0400

    database config

--- Lex/settings.py
+++ Lex/settings.py
@@ -75,7 +75,10 @@ WSGI_APPLICATION = 'Lex.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql'
+        
+    }
 }
 
 
