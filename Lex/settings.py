11f0c5b5d9c0748b4522dbf7c5a11ae4f3aaa1f5
Astralroth <81763817+Astralroth@users.noreply.github.com>
2021-06-07 16:23:08 -0400

    Update settings.py

--- Lex/settings.py
+++ Lex/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cky1s3#dw6zgk54vp@%59^i1@k6$fuznu+00=tkjo=!((6u&!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
