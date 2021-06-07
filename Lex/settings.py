d7a4fd142a4e4e282f5c78925477788ce96453d7
Franco Sandoval <francosandoval003@gmail.com>
2021-06-07 16:53:16 -0400

    database config

--- Lex/settings.py
+++ Lex/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'Lex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DATABASE',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
