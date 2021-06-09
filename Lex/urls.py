df122eceeb423ae8ebdd747e1e1c9c62d23fac02
Franco Sandoval <francosandoval003@gmail.com>
2021-06-09 17:00:20 -0400

    urls config

--- Lex/urls.py
+++ Lex/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('',include('core.urls'))
+    path('core/',include('core.urls'))
 ]
