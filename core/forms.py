0fc0610ac3fc0294e7b5413e8891de5e7f78c459
Constanza Bermudez <coni.bermudez.droguett.az@gmail.com>
2021-06-30 21:16:32 -0400

    hello

--- core/forms.py
+++ core/forms.py
@@ -21,7 +21,7 @@ class SolicitarServicioForm(forms.ModelForm):
     email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Correo electronico'}))
     phone=forms.CharField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Telefono celular'}))
     cause=forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','style':'height: 80px','placeholder':'Redactar causa'}))
-    files=forms.FileField()
+    files=forms.FileField(required=False)
     
     class Meta:
         model = Servicio
@@ -70,11 +70,11 @@ class ContratoForm(forms.ModelForm):
     files_causa=forms.FileField(required=False)
     datetime=forms.DateField(widget=forms.DateInput(attrs={'class':'form-control','type':'date'}))
     type_service=forms.ChoiceField(choices=(('',''),('Familia','Familia'),('Civiles','Civiles'),('Inmobiliarias','Inmobiliarias'),('Salud','Salud'),('Transito','Transito'),),widget=forms.Select(attrs={'class':'form-control'}))
-    additional_service=forms.ChoiceField(choices=(('',''),('Otros','Otros'),),widget=forms.Select(attrs={'class':'form-control'}))
-    format=forms.ChoiceField(choices=(('word','word'),('pdf','pdf')),widget=forms.RadioSelect(),required=False)
+    additional_service=forms.ChoiceField(choices=(('',''),('Otros','Otros'),),required=False, widget=forms.Select(attrs={'class':'form-control'}))
+    # format=forms.ChoiceField(choices=(('word','word'),('pdf','pdf')),widget=forms.RadioSelect(),required=False)
     class Meta:
         model = Contrato
-        fields = ("name","rut","age","email","phone","files_boleta","files_causa","datetime",)
+        fields = ("name","rut","age","email","phone","files_boleta","files_causa","datetime", "type_service", "additional_service",)
     
     def save(self,id,commit=True):
         user=super(ContratoForm,self).save(commit=False)
@@ -85,12 +85,12 @@ class ContratoForm(forms.ModelForm):
 
 class PagosForm(forms.ModelForm):
 
-    name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),label="Nombre del cliente")
-    rut=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label="Rut")
-    date=forms.DateField(widget=forms.DateInput(attrs={'class':'form-control','type':'date'}), label="Fecha")
-    type=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),label="Tipo de pago")
-    mount=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}), label="Monto")
-    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}),label="Correo electronico")
+    name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre Cliente '}))
+    rut=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Rut'}), label="Rut")
+    date=forms.DateField(widget=forms.DateInput(attrs={'class':'form-control','placeholder':'Fecha','type':'date'}))
+    type=forms.ChoiceField(choices=(('',''),('Efectivo','Efectivo'),('Transferencia','Transferencia'),('Cheque','Cheque'),('Crédito','Crédito'),('Débito','Débito'),),widget=forms.Select(attrs={'class':'form-control'}))
+    mount=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Monto '}))
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email '}))
 
     class Meta:
         model = Pagos
@@ -105,11 +105,11 @@ class PagosForm(forms.ModelForm):
 
 class AgregarCausas(forms.ModelForm):
 
-    name=forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control'}),label="Nombre del cliente")
-    age=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}), label="Edad")
-    email=forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'class':'form-control'}),label="Correo electronico")
-    phone=forms.CharField(max_length=12, widget=forms.NumberInput(attrs={'class':'form-control'}), label="Telefono celular")
-    rut=forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class':'form-control'}), label="Rut")
+    name=forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre Cliente'}))
+    age=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Edad'}))
+    email=forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
+    phone=forms.CharField(max_length=12, widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Teléfono'}))
+    rut=forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Rut'}))
     files_boleta=forms.FileField(widget=forms.FileInput, label="Boleta",required=False)
     files_causa=forms.FileField(widget=forms.FileInput, label="Causa", required=False)
     files_contrato=forms.FileField(widget=forms.FileInput, label="Contrato",required=False)
@@ -127,11 +127,11 @@ class AgregarCausas(forms.ModelForm):
 
 class PresupuestoForm(forms.ModelForm):
     
-    name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),label="Nombre del cliente")
-    servicio=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), label="Tipo Servicio")
-    presupuesto=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}),label="Presupuesto")
-    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}), label="Correo Electrónico")
-    rut=forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class':'form-control'}), label="Rut")
+    name=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre Cliente'}))
+    servicio=forms.ChoiceField(choices=(('',''),('Familia','Familia'),('Civiles','Civiles'),('Inmobiliarias','Inmobiliarias'),('Salud','Salud'),('Transito','Transito'),),widget=forms.Select(attrs={'class':'form-control'}))
+    presupuesto=forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control','placeholder':'Presupuesto'}))
+    email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
+    rut=forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Rut'}))
     
     class Meta:
         model = Presupuesto
